2f56ab9e7800d9faeff9b2b6914de3d9d6740953
Romain <romain.lalin@epitech.eu>
2022-06-17 19:37:37 +0200

    Mise en place de la branch et first run

---
+++ src/main/java/net/Ethernyx/stellamecanics/StellamecanicsClient.java
@@ -0,0 +1,11 @@
+package net.Ethernyx.stellamecanics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellamecanicsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
